2244bdf057fb9fe84f034458ebaf8a925e3faee9
Today <ipp.15122022@gmail.com>
2022-10-19 17:16:01 +0900

    [add] Create a code to verify "perror()"

---
+++ src/test/perror.c
@@ -0,0 +1,17 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main() {
+    int sock;
+
+    sock = socket(3000, 4000, 5000);
+    if(sock < 0) {
+        perror("socket");
+        fprintf(stderr, "error: %d\n", errno);
+        return 1;
+    }
+
+    return 0;
+}
