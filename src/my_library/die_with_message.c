cc5dee2437f708e90dc62d390940d79985aa8c88
Today <ipp.15122098@gmail.com>
2022-10-20 13:51:38 +0900

    [update] Create a function "DieWithSystemMessage2()" to specify "errno"

--- src/my_library/die_with_message.c
+++ src/my_library/die_with_message.c
@@ -1,11 +1,18 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "./my_library.h"
 
 void DieWithSystemMessage(const char *msg) {
     perror(msg);
     fprintf(stderr, "errno: %d\n", errno);
+    exit(1);
+}
+
+void DieWithSystemMessage2(const char *msg, int no) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(no));
+    fprintf(stderr, "errno: %d\n", no);
     exit(1);
 }
--- src/my_library/my_library.h
+++ src/my_library/my_library.h
@@ -3,5 +3,6 @@
 
 // Handle error with sys msg.
 void DieWithSystemMessage(const char *);
+void DieWithSystemMessage2(const char *, int);
 
 #endif  // UTILITY_H
