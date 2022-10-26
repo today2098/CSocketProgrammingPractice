5e6c687d006315173c4986daf5c619f3fef24201
Today <ipp.15122098@gmail.com>
2022-10-27 00:15:45 +0900

    [add] Create a project "Simple File Transfer"

---
+++ src/SimpleFT/simple_ft_receiver.c
@@ -0,0 +1,80 @@
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../my_library/my_library.h"
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        fprintf(stderr,
+                "Simple File Transfer (Receiver)\n\n"
+                "Usage:  %s <outputfilename>\n",
+                argv[0]);
+        return 1;
+    }
+
+    const char *OUTPUT_DIR = "output/";
+    int sock0, sock;
+    int fd;
+    char buf0[64], buf[65536];
+    int n, ret;
+
+    // Listen用のソケットを作成する．
+    sock0 = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock0 == -1) DieWithSystemMessage("socket()");
+
+    // ソケットの設定を行う．
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(12345);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    ret = bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+    if(ret == -1) DieWithSystemMessage("bind()");
+
+    // TCPクライアントからの接続要求を待ち受けている状態にする．
+    ret = listen(sock0, 5);
+    if(ret == -1) DieWithSystemMessage("listen()");
+
+    // TCPクライアントからの接続要求を受け付ける．
+    struct sockaddr_in client;
+    memset(&client, 0, sizeof(client));
+    socklen_t len = sizeof(client);
+    sock = accept(sock0, (struct sockaddr *)&client, &len);
+    if(sock == -1) DieWithSystemMessage("accept()");
+
+    // debug.
+    GetAddressFromSockaddr_in(&client, buf0, sizeof(buf0));
+    printf("accept from %s\n", buf0);
+
+    // ファイルを作成する．
+    char filepath[256];
+    memset(filepath, 0, sizeof(filepath));
+    snprintf(filepath, sizeof(filepath), "%s%s", OUTPUT_DIR, argv[1]);
+    MakeDirectory(filepath);
+    fd = open(filepath, O_WRONLY | O_CREAT, 0600);
+    if(fd == -1) DieWithSystemMessage("open()");
+
+    // データを受信する．
+    int cnt = 1;
+    memset(buf, 0, sizeof(buf));
+    while((n = read(sock, buf, sizeof(buf))) > 0) {
+        ret = write(fd, buf, n);
+        if(ret < 1) DieWithSystemMessage("write()");
+        printf("[%d] send %d Byte\n", cnt++, n);
+    }
+    if(n == -1) DieWithSystemMessage("read()");
+
+    // TCPセッションを終了する．
+    close(sock);
+
+    // Listen用socketを終了する．
+    close(sock0);
+
+    return 0;
+}
---
+++ src/SimpleFT/simple_ft_sender.c
@@ -0,0 +1,64 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../my_library/my_library.h"
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        fprintf(stderr,
+                "Simple File Transfer (Sender)\n\n"
+                "Usage:  %s <filename>\n",
+                argv[0]);
+        return 1;
+    }
+
+    int fd;
+    int sock;
+    char buf0[64], buf[65536];
+    int n, ret;
+
+    // 送信ファイルを開く．
+    fd = open(argv[1], O_RDONLY);
+    if(fd == -1) DieWithSystemMessage("open()");
+
+    // ソケットを作成する．
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1) DieWithSystemMessage("socket()");
+
+    // 接続先指定用構造体の準備．
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(12345);
+    ret = inet_pton(AF_INET, "127.0.0.1", &server.sin_addr.s_addr);
+    if(ret <= 0) DieWithSystemMessage("inet_pton()");
+
+    // サーバに接続する．
+    ret = connect(sock, (struct sockaddr *)&server, sizeof(server));
+    if(ret == -1) DieWithSystemMessage("connect()");
+
+    // debug.
+    GetAddressFromSockaddr_in(&server, buf0, sizeof(buf0));
+    printf("connect to %s\n", buf0);
+
+    // データを送信する．
+    int cnt = 1;
+    memset(buf, 0, sizeof(buf));
+    while((n = read(fd, buf, sizeof(buf))) > 0) {
+        ret = write(sock, buf, n);
+        if(ret < 1) DieWithSystemMessage("write()");
+        printf("[%d] send %d Byte\n", cnt++, n);
+    }
+    if(n == -1) DieWithSystemMessage("read()");
+
+    // TCPセッションを終了する．
+    close(sock);
+
+    return 0;
+}
