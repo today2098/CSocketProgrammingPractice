d0b0916e4dd779b4a6644c9721f19c2b4e18cea7
Today <ipp.15122098@gmail.com>
2022-10-27 00:36:59 +0900

    [clean] Refactoring

--- src/test/getaddrinfo.c
+++ src/test/getaddrinfo.c
@@ -6,33 +6,32 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
-    if(argc > 2) {
+    if(argc != 2) {
         fprintf(stderr,
                 "Name Resolution\n\n"
-                "Usage:\n\t%s [hostname]\n\n"
+                "Usage:\n\t%s <hostname>\n\n"
                 "Example:\n\t%s localhost\n\t%s google.com\n\t%s sony.jp\n",
                 argv[0], argv[0], argv[0], argv[0]);
         return 1;
     }
 
-    char *hostname = (argc >= 2 ? argv[1] : "localhost");
+    char *hostname = argv[1];
+    struct addrinfo hints, *res0, *res;
+    struct in_addr addr;
+    char buf[16];
+    int ret;
 
-    struct addrinfo hints, *res0;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    int ret = getaddrinfo(hostname, NULL, &hints, &res0);
+    ret = getaddrinfo(hostname, NULL, &hints, &res0);
     if(ret != 0) {
         printf("error: %s\n", gai_strerror(ret));
         return 1;
     }
 
-    struct addrinfo *res;
     for(res = res0; res; res = res->ai_next) {
-        struct in_addr addr;
         addr.s_addr = ((struct sockaddr_in *)(res->ai_addr))->sin_addr.s_addr;
-
-        char buf[16];
         inet_ntop(AF_INET, &addr, buf, sizeof(buf));
         printf("IP address: %s\n", buf);
     }
