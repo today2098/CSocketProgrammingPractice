429adc9af5299dcd45d07ce469b369b3a3ede549
Today <ipp.15122098@gmail.com>
2022-10-27 15:00:03 +0900

    [update] Add "fflush()" after "fprintf()"

--- src/my_library/die_with_message.c
+++ src/my_library/die_with_message.c
@@ -1,18 +1,20 @@
 #include "./die_with_message.h"
 
 #include <errno.h>   // for errno.
-#include <stdio.h>   // for stderr, fprintf(), perror().
+#include <stdio.h>   // for stderr, fprintf(), fflush() and perror().
 #include <stdlib.h>  // for exit().
 #include <string.h>  // strerror().
 
 void DieWithSystemMessage(const char *msg) {
     perror(msg);
     fprintf(stderr, "errno: %d\n", errno);
+    fflush(stderr);
     exit(1);
 }
 
 void DieWithSystemMessage2(const char *msg, int no) {
     fprintf(stderr, "%s: %s\n", msg, strerror(no));
     fprintf(stderr, "errno: %d\n", no);
+    fflush(stderr);
     exit(1);
 }
