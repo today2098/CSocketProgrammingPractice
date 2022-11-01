8e6dbb81e0be6fcdec6fde3edfb2ef8f102e0946
Today <ipp.15122098@gmail.com>
2022-11-02 02:08:09 +0900

    [fix] Fix extern declaration

--- src/my_library/get_address.c
+++ src/my_library/get_address.c
@@ -8,14 +8,14 @@
 
 const unsigned MY_ADDRSTRLEN = 64;
 
-void GetAddressFromSockaddr_in(struct sockaddr_in *addr, char *buf, size_t len) {
+void GetAddressFromSockaddr_in(struct sockaddr_in *saddr, char *buf, size_t len) {
     assert(len >= MY_ADDRSTRLEN);
 
     char buf2[MY_ADDRSTRLEN];
     memset(buf2, 0, sizeof(buf2));
-    inet_ntop(addr->sin_family, &(addr->sin_addr), buf2, sizeof(buf2));
+    inet_ntop(saddr->sin_family, &(saddr->sin_addr), buf2, sizeof(buf2));
 
     memset(buf, 0, len);
-    if(addr->sin_family == AF_INET6) snprintf(buf, len, "[%s]:%d", buf2, ntohs(addr->sin_port));
-    else snprintf(buf, len, "%s:%d", buf2, ntohs(addr->sin_port));
+    if(saddr->sin_family == AF_INET6) snprintf(buf, len, "[%s]:%d", buf2, ntohs(saddr->sin_port));
+    else snprintf(buf, len, "%s:%d", buf2, ntohs(saddr->sin_port));
 }
--- src/my_library/get_address.h
+++ src/my_library/get_address.h
@@ -4,7 +4,9 @@
 #include <netinet/in.h>
 #include <stdio.h>
 
+extern const unsigned MY_ADDRSTRLEN;
+
 // Get address from sockaddr_in by string.
-void GetAddressFromSockaddr_in(struct sockaddr_in *addr, char *buf, size_t len);
+void GetAddressFromSockaddr_in(struct sockaddr_in *saddr, char *buf, size_t len);
 
 #endif  // GET_ADDRESS_H
