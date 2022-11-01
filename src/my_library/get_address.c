49f54c59e77ff53e559fa8e36c32e9ad25331d2d
Today <ipp.15122098@gmail.com>
2022-11-01 23:16:59 +0900

    [update] Adapt to the notation of IPv6

--- src/my_library/get_address.c
+++ src/my_library/get_address.c
@@ -1,16 +1,21 @@
-#include "./get_address.h"
+#include "get_address.h"
 
-#include <arpa/inet.h>   // for inet_ntop().
-#include <assert.h>      // for assert().
-#include <netinet/in.h>  // for sockaddr_in and ntohs().
-#include <stdio.h>       // snprintf().
-#include <string.h>      // for memset().
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+
+const unsigned MY_ADDRSTRLEN = 64;
 
 void GetAddressFromSockaddr_in(struct sockaddr_in *addr, char *buf, size_t len) {
-    assert(len >= 64);
-    char buf2[64];
+    assert(len >= MY_ADDRSTRLEN);
+
+    char buf2[MY_ADDRSTRLEN];
     memset(buf2, 0, sizeof(buf2));
     inet_ntop(addr->sin_family, &(addr->sin_addr), buf2, sizeof(buf2));
+
     memset(buf, 0, len);
-    snprintf(buf, len, "%s:%d", buf2, ntohs(addr->sin_port));
+    if(addr->sin_family == AF_INET6) snprintf(buf, len, "[%s]:%d", buf2, ntohs(addr->sin_port));
+    else snprintf(buf, len, "%s:%d", buf2, ntohs(addr->sin_port));
 }
