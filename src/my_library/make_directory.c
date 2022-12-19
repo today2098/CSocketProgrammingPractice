846ccd11f48bdca79684d0be04f59d1344b59c2f
Today <ipp.15122098@gmail.com>
2022-12-19 22:16:24 +0900

    [fix] Change type of argument

--- src/my_library/make_directory.c
+++ src/my_library/make_directory.c
@@ -1,17 +1,20 @@
 #include "./make_directory.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 void MakeDirectory(const char *dirpath) {
-    int i;
+    assert(strlen(dirpath) <= 256);
 
+    int i;
+    int len = strlen(dirpath);
     char tmp[256];
+
     memset(tmp, 0, sizeof(tmp));
     snprintf(tmp, sizeof(tmp), "%s", dirpath);
-    size_t len = strlen(dirpath);
     for(i = len - 1; i >= 0; --i) {
         if(tmp[i] == '/') break;
         tmp[i] = 0;
@@ -19,7 +22,7 @@ void MakeDirectory(const char *dirpath) {
     for(i = 0; i < (int)len && tmp[i]; ++i) {
         if(tmp[i] == '/') {
             tmp[i] = 0;
-            mkdir(tmp, 0777);
+            mkdir(tmp, 0755);
             tmp[i] = '/';
         }
     }
--- src/my_library/make_directory.h
+++ src/my_library/make_directory.h
@@ -1,7 +1,7 @@
 #ifndef MAKE_DIRECTORY_H
 #define MAKE_DIRECTORY_H 1
 
-// Make directory. (Path need '/' at end.)
+// Make directory (you need '/' at end of path).
 void MakeDirectory(const char *dirpath);
 
 #endif  // MAKE_DIRECTORY_H
