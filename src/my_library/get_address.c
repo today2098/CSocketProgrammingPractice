e7a13754369e7f89006e963c2a7221595f559ca6
Today <ipp.15122098@gmail.com>
2022-12-21 09:19:04 +0900

    [update] Add error handling

--- src/my_library/get_address.c
+++ src/my_library/get_address.c
@@ -6,16 +6,25 @@
 #include <stdio.h>
 #include <string.h>
 
-const unsigned MY_ADDRSTRLEN = 64;
+#include "print_sys_err_msg.h"
 
-void GetAddressFromSockaddr_in(const struct sockaddr_in *src, char *dst, size_t size) {
+const size_t MY_ADDRSTRLEN = 64;
+
+int GetAddressFromSockaddr_in(const struct sockaddr_in *src, char *dst, size_t size) {
     assert(size >= MY_ADDRSTRLEN);
+    int res;
 
-    char buf[MY_ADDRSTRLEN];
+    char buf[INET6_ADDRSTRLEN];
     memset(buf, 0, sizeof(buf));
     inet_ntop(src->sin_family, &src->sin_addr, buf, sizeof(buf));
+    if(buf == NULL) {
+        PrintSystemErrorMessage("inet_ntop()");
+        return -1;
+    }
 
     memset(dst, 0, size);
-    if(src->sin_family == AF_INET6) snprintf(dst, size, "[%s]:%d", buf, ntohs(src->sin_port));
-    else snprintf(dst, size, "%s:%d", buf, ntohs(src->sin_port));
+    if(src->sin_family == AF_INET) snprintf(dst, size, "%s:%d", buf, ntohs(src->sin_port));
+    else if(src->sin_family == AF_INET6) snprintf(dst, size, "[%s]:%d", buf, ntohs(src->sin_port));
+    else fprintf(stderr, "adress family is unsupported");
+    return 0;
 }
--- src/my_library/get_address.h
+++ src/my_library/get_address.h
@@ -4,9 +4,9 @@
 #include <netinet/in.h>
 #include <stdio.h>
 
-extern const unsigned MY_ADDRSTRLEN;
+extern const size_t MY_ADDRSTRLEN;
 
 // Get address from sockaddr_in by string.
-void GetAddressFromSockaddr_in(const struct sockaddr_in *src, char *dst, size_t size);
+int GetAddressFromSockaddr_in(const struct sockaddr_in *src, char *dst, size_t size);
 
 #endif  // GET_ADDRESS_H
